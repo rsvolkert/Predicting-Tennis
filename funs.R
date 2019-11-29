5b925fa2b180b46cfc272da6a11a4e950503e304
Ryan Volkert <rvolk75@gmail.com>
2019-11-29 11:44:29 -0700

    Initialize funs

---
+++ funs.R
@@ -0,0 +1,13 @@
+# Probability that A wins rally given A serves
+## 1 - pr = qr = prob B wins given A serves
+pr <- function(pwon, pserve) {
+  mean(pwon / pserve)
+}
+
+# Probability that A wins game given A serves
+## 1 - pg = qg = prob B wins given A serves
+pg <- function(pr) {
+  qr <- 1 - pr
+  
+  pr^4 * (1 + 4*qr + 10*qr^2) + 10*(pr*qr)^3 * pr^2 * (1 - 2*p2*qr)^-1
+}
