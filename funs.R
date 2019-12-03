3a3bd54b14a7daf3675e0f34b4f4b4d4f4e23be0
Olivia Beck <55454718+olbeck@users.noreply.github.com>
2019-12-02 21:01:13 -0700

    Add set function

--- funs.R
+++ funs.R
@@ -1,3 +1,7 @@
+
+library(numbers)
+
+
 # Probability that A wins rally given A serves
 ## 1 - pr = qr = prob B wins given A serves
 pr <- function(pwon, pserve) {
@@ -11,3 +15,39 @@ pg <- function(pr) {
   
   pr^4 * (1 + 4*qr + 10*qr^2) + 10*(pr*qr)^3 * pr^2 * (1 - 2*p2*qr)^-1
 }
+
+
+#Prob A wins a set given A served
+pASij <- function(i, j, pAG, pBG){
+  if(i == 0 & j == 1){return(1)}
+  if(i < 0){return(0)}
+  if(j < 0){return(0)}
+  for( a in 1:i){ 
+    for( b in j:6){
+      if(mod((i+j-1), 1) == 1) {
+        if(j == 6 & i <= 5){
+          return(pASij(i, j-1, pAG, pBG)*(1-pAG))
+        }
+        else if( i == 6 & j <= 5){
+          return(pASij(i-1, j, pAG, pBG)*pAG)
+        }
+        else{
+          return(pASij(i-1, j, pAG, pBG)*pAG + pASij(i, j-1, pAG, pBG)*(1-pAG))
+        }
+      }
+      else{
+        if(j == 6 & i <= 5){
+          return(pASij(i, j-1, pAG, pBG)*pBG)
+        }
+        else if( i == 6 & j <= 5){
+          return(pASij(i-1, j, pAG, pBG)*(1-pBG))
+        }
+        else{
+          return(pASij(i-1, j, pAG, pBG)*(1-pBG) + pASij(i, j-1, pAG, pBG)*pBG)
+        }
+      }
+    }
+  }
+  
+}
+
