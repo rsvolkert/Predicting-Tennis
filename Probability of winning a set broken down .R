8589e27abf338b46ac1aad90190e21403bdde15a
Emma Lewis <eklewis@rams.colostate.edu>
2019-12-10 22:28:47 -0700

    Updated

--- Probability of winning a set broken down .R
+++ Probability of winning a set broken down .R
@@ -2,27 +2,27 @@ pga= 0.5
 qga= 1-pga
 
 pgb=0.5
-qga=1-pgb
+qgb=1-pgb
 
-A1=(pga*qga)^3
+A1=(pga*qgb)^3
 
-A2=3*(pga)^3*(qga)*(qbg)^3
+A2=3*(pga)^3*(qga)*(qgb)^3
   +3*(pga)^4*(pgb)*(qgb)^2
 
 A3=12*(pga)^3*(qga)*(pgb)*(qgb)^3
   +6*(pga)^2*(qga)^2*(qgb)^4
   +3*(pga)^4*(pgb)^2*(qgb)^2
 
-A4=24*(pga)^3*(qga)^2*(pgb)*qgb^3
+A4=24*(pga)^3*(qga)^2*(pgb)*(qgb)^3
   +24*(pga)^4*(qga)*(pgb)^2*(qgb)^2
-  +4*(pga)^2*(qga)^3*(qga)^4
+  +4*(pga)^2*(qga)^3*(qgb)^4
   +4*(pga)^5*(pgb)^3*(qgb)
 
 A5=60*(pga)^3*(qga)^2*(pgb)^2*(qgb)^3
   +40*(pga)^2*(qga)^3*(pgb)*(qga)^4
   +20*(pga)^4*(qga)*(pgb)^3*(qgb)^2
   +5*(pga)*(qga)^4*(qgb)^5
-  +(pga)^5*(pga)^4*(qgb)
+  +(pga)^5*(pgb)^4*(qgb)
 
 A6=100*(pga)^3*(qga)^3*(pgb)^2*(qgb)^4
 +100*(pga)^4*(qga)^2*(pgb)^3*(qgb)^3
